d4b46e2bdf5ada23c688981be0cedd0239a4d9d1
Snebabe <eliassoderberg03@gmail.com>
2024-09-24 21:08:05 +0200

    lite onödiga kommentarer

--- site/src/lib/bed_scraper.py
+++ site/src/lib/bed_scraper.py
@@ -3,26 +3,30 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 
-driver = webdriver.Chrome()
-
+all_beds = []
 url = 'https://www.ikea.com/se/en/cat/beds-bm003/'
 
-all_beds = []
-
+driver = webdriver.Chrome()
 driver.get(url)
 
-print("Sleeping")
-sleep(2)
+
+# For page to load
+sleep(2) 
+
+# Remove cookies
 cookies = driver.find_element(By.ID, 'onetrust-accept-btn-handler')
 cookies.click()
 
-button = driver.find_element(By.XPATH, '//span[@class="plp-btn__label"]/../..')
+show_more_button = driver.find_element(By.XPATH, '//span[@class="plp-btn__label"]/../..')
+click_times = 70
 
-for i in range(70):
-  sleep(1.5)
-  button.click()
-  print(f"clicked {i+1} times")
+# Load more beds to page
+for i in range(click_times):
+  sleep(0.2)
+  show_more_button.click()
+  print(f"clicked {i+1}/{click_times} times")
 
+# Appends beds as dicts. If any field == null it is not included
 def scrape_products():
 
   products = driver.find_elements(By.CLASS_NAME, 'plp-fragment-wrapper')
@@ -31,6 +35,7 @@ def scrape_products():
   
   for product in products:
     try:
+      # Slower with XPATH but didn't get .CLASS to work (????)
       name = product.find_element(By.XPATH, './/span[@class="notranslate plp-price-module__product-name"]').text
       description = product.find_element(By.XPATH, './/span[@class="plp-price-module__description"]').text
       image = product.find_element(By.XPATH, './/img[@class="plp-image plp-product__image"]').get_attribute('src')
@@ -42,7 +47,7 @@ def scrape_products():
         'name': name,
         'description': description,
         'image': image,
-        'price': price,
+        'price': int(price),
         'springs': springs,
         'firmness': firmness,
       })
@@ -54,15 +59,13 @@ def scrape_products():
     i += 1
 
 scrape_products()
-
-for bed in all_beds:
-  print(bed)
-
 driver.quit()
 
+# Find absolute path so file_path points to the same folder as scraper
 script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir, 'output.txt')
 
+# One bed per line, many beds are the same model with different dimensions
 with open(file_path, 'w', encoding='utf-8') as file:
   for bed in all_beds:
     file.write(f'{bed}\n')

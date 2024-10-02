79d4333037fbd09466e3cbd981acf51efb6ab989
Sigge Rajamäe <siggerajamae@gmail.com>
2024-10-02 20:28:58 +0200

    Pitch revision 1

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,27 +1,29 @@
 <script>
     import ToolCard from "./ToolCard.svelte";
-    import nightlightIcon from "$lib/assets/nightlight.svg";
-    import botIcon from "$lib/assets/bot.svg";
-    import closetIcon from "$lib/assets/checkroom.svg";
-    import gogglesIcon from "$lib/assets/goggles.svg";
+
+    // Icons
+    import NightlightIcon from "$lib/components/NightlightIcon.svelte";
+    import BotIcon from "$lib/components/BotIcon.svelte";
+    import CheckroomIcon from "$lib/components/CheckroomIcon.svelte";
+    import GogglesIcon from "$lib/components/GogglesIcon.svelte";
 </script>
 
 <main>
-    <h1>Sleep with IKEA</h1>
+    <h1>IKEA Comfort Zone</h1>
     <section>
         <h2>Difficulties sleeping?</h2>
         <p>
             You're not alone. Millions of adults struggle with sleep issues
-            every night, with about 1 in 3 experiencing insomnia or difficulty
-            falling and staying asleep. From stress and anxiety to irregular
-            routines and lifestyle choices, many factors can affect your ability
-            to rest. Poor sleep not only leads to fatigue but also impacts mood,
-            productivity, and long-term health. Whether you're tossing and
-            turning or waking up frequently during the night, finding a solution
-            to improve your sleep quality is essential for your overall
-            well-being. One of our many goals at IKEA is to help our customers
-            with improving their sleep. Explore some of our tools below,
-            designed to make you comfortable.
+            every night, with about one in three experiencing insomnia or
+            difficulty falling and staying asleep. From stress and anxiety to
+            irregular routines and lifestyle choices, many factors can affect
+            your ability to rest. Poor sleep not only leads to fatigue but also
+            impacts mood, productivity, and long-term health. Whether you're
+            tossing and turning or waking up frequently during the night,
+            finding a solution to improve your sleep quality is essential for
+            your overall well-being. One of our many goals at IKEA is to help
+            our customers with improving their sleep. Explore some of our tools
+            below, designed to make you comfortable.
         </p>
     </section>
     <section>
@@ -31,42 +33,140 @@
                 name={"Dream Planner"}
                 description={`Get help furnishing your room for your specific
                 sleeping needs.`}
-                icon={nightlightIcon}
+                icon={NightlightIcon}
                 href={"#"}
             />
             <ToolCard
                 name={"Slumber Bot"}
                 description={`Knows everything there is to know about sleeping,
                 and how our different products affect sleep!`}
-                icon={botIcon}
+                icon={BotIcon}
                 href={"slumberbot"}
             />
             <ToolCard
                 name={"Night Closet"}
                 description={`This is not a portal to Narnia, but perhaps
                 something even better?`}
-                icon={closetIcon}
+                icon={CheckroomIcon}
                 href={"#"}
             />
             <ToolCard
                 name={"Moon Goggles"}
                 description={`Stare into the sun for too long and you go blind,
                 but what about the moon?`}
-                icon={gogglesIcon}
+                icon={GogglesIcon}
                 href={"#"}
             />
         </div>
     </section>
     <section>
-        <h2>Improving Sleep in Asia</h2>
-        <p>TODO</p>
+        <h2>Improving sleep in Asia</h2>
+        <section>
+            <h3>Design principles</h3>
+            <section>
+                <h4>Usability</h4>
+                <p>
+                    Our tools are designed with young East Asians in mind, but
+                    are generally targeted towards anyone who seeks tips or
+                    inspiration on how to improve their sleep comfort. In our
+                    research, we found that a significant portion of the
+                    population in countries such as South Korea and Japan
+                    struggle with sleep, where some primary factors are stress,
+                    noise and air pollution and heat.
+                </p>
+                <p>
+                    IKEA offers products that can help with these issues, and we
+                    aim to create a space where users can easily discover
+                    existing solutions tailored to their individual needs. With
+                    this solution we hope to bring a more interactive experience
+                    over IKEA's existing web pages with general information on
+                    sleep and product recommendations.
+                </p>
+            </section>
+            <section>
+                <h4>Feasibility</h4>
+                <p>
+                    For our innovation, we have decided to work almost
+                    exclusively with the web framework SvelteKit. Svelte
+                    provides a straightforward developer experience for creating
+                    and deploying web pages, and due to the relatively simple
+                    nature of the tools themselves, writing our backend in
+                    TypeScript is feasible. The innovation does not require any
+                    special data beyond the product information available on
+                    IKEA's own website and user information provided by the
+                    users themselves. The product data is collected, and each
+                    product is automatically annotated with tags that can then
+                    be used to train models for product recommendations. Since
+                    user information may include sensitive data such as location
+                    details, this data must be handled with care.
+                </p>
+            </section>
+            <section>
+                <h4>Viability</h4>
+                <p>
+                    Our innovation aligns with IKEA's vision of improving living
+                    by enhancing the shopping experience with interactive tools
+                    focused on improving sleep. We believe that innovation is
+                    viable, and can be implemented in reasonable a time frame,
+                    without the burden of maintenance, as the software easily
+                    could be designed to update itself when new products are
+                    added to the catalog. For this reason, the idea is
+                    essentially without risk, simply improving customer
+                    experience.
+                </p>
+            </section>
+        </section>
+        <section>
+            <h3>Demonstration</h3>
+            <p>
+                As of writing this, the general idea of Slumber Bot and Dream
+                Planner should be communicated through what has been implemented
+                so far. Click on the card for either of these tools to navigate
+                to their respective pages. Night Closet and Moon Goggles are
+                still just placeholders, and we are exploring the idea of a
+                catalog-sleeping-podcast.
+            </p>
+        </section>
+    </section>
+    <section id="section-team">
+        <h2>Our team</h2>
+        <div id="team-members">
+            <section>
+                <img src="/team/andersson.jpg" alt="Joshua Andersson" />
+                <h3>Joshua Andersson</h3>
+                <a href="mailto:jeffmar@student.chalmers.se"
+                    >jeffmar@student.chalmers.se</a
+                >
+            </section>
+            <section>
+                <img src="/team/hulthe.jpg" alt="Rasmus Hulthe" />
+                <h3>Rasmus Hulthe</h3>
+                <a href="mailto:rasmushu@student.chalmers.se"
+                    >rasmushu@student.chalmers.se</a
+                >
+            </section>
+            <section>
+                <img src="/team/rajamae.jpg" alt="Sigge Rajamäe" />
+                <h3>Sigge Rajamäe</h3>
+                <a href="mailto:siggera@student.chalmers.se"
+                    >siggera@student.chalmers.se</a
+                >
+            </section>
+            <section>
+                <img src="/team/soderberg.jpg" alt="Elias Söderberg" />
+                <h3>Elias Söderberg</h3>
+                <a href="elisod@student.chalmers.se"
+                    >elisod@student.chalmers.se</a
+                >
+            </section>
+        </div>
     </section>
 </main>
 
 <style lang="scss">
     main {
         color: var(--color-text-main);
-        padding: 1rem;
+        padding: 2rem;
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -74,6 +174,14 @@
 
     section {
         width: 40rem;
+        margin-top: 1.2rem;
+        margin-bottom: 1.2rem;
+    }
+
+    #section-team {
+        h2 {
+            text-align: center;
+        }
     }
 
     #tools {
@@ -81,5 +189,37 @@
         grid-template-columns: repeat(2, 1fr);
         grid-template-rows: repeat(2, 1fr);
         gap: 1.1rem;
+    }
+
+    #team-members {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        gap: 3rem;
+        padding: 2rem;
+
+        section {
+            width: auto;
+            margin: 0;
+            text-align: center;
+            overflow: hidden;
+
+            img {
+                aspect-ratio: 1;
+                width: 70%;
+                object-fit: cover;
+                object-position: center;
+                border-radius: 50%;
+            }
+
+            h3 {
+                font-size: 1.2em;
+                margin-bottom: 0.3em;
+            }
+
+            a {
+                font-size: 0.8em;
+            }
+        }
     }
 </style>
--- site/src/routes/slumberbot/+page.svelte
+++ site/src/routes/slumberbot/+page.svelte
@@ -1,10 +1,10 @@
 <script>
     import OpenAI from "openai";
 
-    let textInput = ''; // This will hold the value of the textbox
+    let textInput = ""; // This will hold the value of the textbox
     let chatHistory = [];
     let waitingForResponse = false;
-    
+
     async function handleSubmit() {
         if (!textInput.trim()) {
             return;
@@ -15,36 +15,57 @@
 
         // Add input to chat history
         chatHistory = [...chatHistory, { role: "user", content: textInput }];
-        textInput = ''; // Clear the input after adding it to chat history
+        textInput = ""; // Clear the input after adding it to chat history
 
         // Set up OpenAI API client
-        const openai = new OpenAI({ 
-            dangerouslyAllowBrowser: true, 
-            apiKey: '...'
+        const openai = new OpenAI({
+            dangerouslyAllowBrowser: true,
+            apiKey: "...",
         });
 
         try {
-            const res = await fetch('https://api.openai.com/v1/chat/completions', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${openai.apiKey}`
+            const res = await fetch(
+                "https://api.openai.com/v1/chat/completions",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${openai.apiKey}`,
+                    },
+                    body: JSON.stringify({
+                        model: "gpt-4",
+                        messages: [
+                            {
+                                role: "system",
+                                content: `You are a bot whose goal is to help
+                                         people sleep. Your owner is IKEA. You should
+                                         only discuss things that could help people
+                                         sleep, our things about IKEA.`,
+                            },
+                            {
+                                role: "system",
+                                content: `You are not ChatGPT and are not
+                                         created by OpenAI, and you shall not make any
+                                         mention of that.`,
+                            },
+                            {
+                                role: "system",
+                                content: `Start the conversation by asking the user
+                                          why they think they are having trouble
+                                          falling asleep, and then help them from
+                                          there. Do not discuss IKEA products unless
+                                          the user makes a mention of them.`,
+                            },
+                            ...chatHistory,
+                        ],
+                        stream: true,
+                    }),
                 },
-                body: JSON.stringify({
-                    model: "gpt-4",
-                    messages: [
-                        { role: "system", content: "You are a bot whose goal is to help people sleep. Your owner is IKEA. You should only discuss things that could help people sleep, our things about IKEA." },
-                        { role: "system", content: "You are not ChatGPT and are not created by OpenAI, and you shall not make any mention of that." },
-                        { role: "system", content: "Start the conversation by asking the user why they think they are having trouble falling asleep, and then help them from there. Do not discuss IKEA products unless the user makes a mention of them." },
-                        ...chatHistory,
-                    ],
-                    stream: true
-                })
-            });
+            );
 
             const reader = res.body.getReader();
-            const decoder = new TextDecoder('utf-8');
-            let assistantResponse = '';
+            const decoder = new TextDecoder("utf-8");
+            let assistantResponse = "";
 
             let old_chatHistory = chatHistory;
             // Read the responses in a stream
@@ -56,28 +77,25 @@
                 const whole_chunk = decoder.decode(value);
                 let split_chunk = whole_chunk.split("data: ");
                 split_chunk.shift();
-                split_chunk.forEach((chunk) =>
-                {
+                split_chunk.forEach((chunk) => {
                     // Don't crash when it sends [DONE]
                     if (chunk.includes("[DONE]")) return;
-                    let cut_chunk = chunk.split("\n")[0].replace("data: ", "")
-                    
-                    let chunk_data = JSON.parse(cut_chunk)
-                    let content = chunk_data.choices[0].delta.content
+                    let cut_chunk = chunk.split("\n")[0].replace("data: ", "");
+
+                    let chunk_data = JSON.parse(cut_chunk);
+                    let content = chunk_data.choices[0].delta.content;
                     if (content == null) return;
                     assistantResponse += content;
                 });
-                
 
-                chatHistory = [...old_chatHistory, { role: "assistant", content: assistantResponse }];
-
-            };
-        }
-        catch (error) {
+                chatHistory = [
+                    ...old_chatHistory,
+                    { role: "assistant", content: assistantResponse },
+                ];
+            }
+        } catch (error) {
             console.error("Error with API call:", error);
-        }
-        finally
-        {
+        } finally {
             waitingForResponse = false;
         }
     }
@@ -85,105 +103,125 @@
     import { onMount } from "svelte";
 
     onMount(() => {
-        document.addEventListener('keydown', function(event) {
-            if (event.key === 'Enter') {
+        document.addEventListener("keydown", function (event) {
+            if (event.key === "Enter") {
                 handleSubmit();
             }
         });
         setTimeout(() => {
-            chatHistory = [{role: "assistant", content: "Hello! How can I assist you today? Are you having trouble sleeping?"}];
+            chatHistory = [
+                {
+                    role: "assistant",
+                    content:
+                        "Hello! How can I assist you today? Are you having trouble sleeping?",
+                },
+            ];
         }, 500);
     });
-
 </script>
 
 <!-- HTML structure -->
 <div id="chat-wrapper">
     <div id="chat-container">
         {#each chatHistory as message}
-            <div class={message.role === 'user' ? 'user-message' : 'assistant-message'}>
-                <p><strong>{message.role === 'user' ? 'You: ' : 'Slumberbot: '}</strong>{message.content}</p>
+            <div
+                class={message.role === "user"
+                    ? "user-message"
+                    : "assistant-message"}
+            >
+                <p>
+                    <strong
+                        >{message.role === "user"
+                            ? "You: "
+                            : "Slumberbot: "}</strong
+                    >{message.content}
+                </p>
             </div>
         {/each}
     </div>
     <div id="input-container">
-        <input type="text" bind:value={textInput} placeholder="Enter your message..." />
+        <input
+            type="text"
+            bind:value={textInput}
+            placeholder="Enter your message..."
+        />
         <button on:click={handleSubmit}>Submit</button>
     </div>
 </div>
 
 <style>
-#chat-wrapper {
-    display: flex;
-    flex-direction: column;
-    height: 500px;
-    max-width: 800px;
-    margin: 0 auto;
-    font-family: 'Arial', sans-serif;
-}
+    #chat-wrapper {
+        display: flex;
+        flex-direction: column;
+        height: 500px;
+        max-width: 800px;
+        margin: 0 auto;
+        font-family: "Arial", sans-serif;
+    }
 
-#chat-container {
-    flex-grow: 1;
-    overflow-y: auto;
-    padding: 20px;
-    background-color: #292929;
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-}
+    #chat-container {
+        flex-grow: 1;
+        overflow-y: auto;
+        padding: 20px;
+        background-color: #292929;
+        border-radius: 10px;
+        display: flex;
+        flex-direction: column;
+    }
 
-.user-message, .assistant-message {
-    max-width: 75%;
-    padding: 10px 15px;
-    border-radius: 15px;
-    margin: 8px 0;
-    font-size: 0.95rem;
-    line-height: 1.4;
-    background-color: #424242;
-    color: white;
-    white-space: pre-line;
-}
+    .user-message,
+    .assistant-message {
+        max-width: 75%;
+        padding: 10px 15px;
+        border-radius: 15px;
+        margin: 8px 0;
+        font-size: 0.95rem;
+        line-height: 1.4;
+        background-color: #424242;
+        color: white;
+        white-space: pre-line;
+    }
 
-.user-message {
-    align-self: flex-end;
-    text-align: right;
-}
+    .user-message {
+        align-self: flex-end;
+        text-align: right;
+    }
 
-.assistant-message {
-    align-self: flex-start;
-    text-align: left;
-}
+    .assistant-message {
+        align-self: flex-start;
+        text-align: left;
+    }
 
-#input-container {
-    display: flex;
-    padding: 10px;
-    background-color: #292929;
-    border-radius: 10px;
-    margin-top: 10px;
-}
+    #input-container {
+        display: flex;
+        padding: 10px;
+        background-color: #292929;
+        border-radius: 10px;
+        margin-top: 10px;
+    }
 
-input {
-    background-color: #424242;
-    color: white;
-    flex-grow: 1;
-    padding: 10px;
-    border: 1px solid black;
-    border-radius: 5px;
-    font-size: 1rem;
-    margin-right: 10px;
-}
+    input {
+        background-color: #424242;
+        color: white;
+        flex-grow: 1;
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 5px;
+        font-size: 1rem;
+        margin-right: 10px;
+    }
 
-button {
-    padding: 10px 20px;
-    background-color: #424242;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 1rem;
-}
+    button {
+        padding: 10px 20px;
+        background-color: #424242;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 1rem;
+    }
 
-button:hover {
-    background-color: #0056b3;
-}
+    button:hover {
+        background-color: #0056b3;
+    }
 </style>
